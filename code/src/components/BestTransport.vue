74434507c334c211a8e6a1766d68ea563f46e368
NivDantas <102577086+NivDantas@users.noreply.github.com>
2023-06-09 01:04:30 -0400

    Add files via upload
    Fetch nos dados da API e retorno das cidades da API para o objeto cidades.

--- code/src/components/BestTransport.vue
+++ code/src/components/BestTransport.vue
@@ -5,6 +5,19 @@
         <b>{{ appName }}</b>
       </b-navbar-brand>
     </b-navbar>
+
+    <div class="form-group">
+      <label for="peso">Peso do frete:</label>
+      <input type="number" id="peso" class="form-control" v-model="pesoFrete">
+    </div>
+
+    <div class="form-group">
+      <label for="city">Cidade de destino:</label>
+      <select id="city" class="form-control" v-model="cidadeSelecionada">
+        <option v-for="city in cidades" :key="city"> {{ city }}</option>
+      </select>
+    </div>
+    <button class="btn btn-primary" @click="methodFoo">Analisar</button>
   </div>
 
 </template>
@@ -25,17 +38,39 @@ export default {
 
     return {
       appName,
+      pesoFrete: 0,
+      cidadeSelecionada: '',
+      cidades: [],
+      freteBarato: null,
+      freteRapido: null
     }
   },
   created() {
-    // Implemente aqui o GET dos dados da API REST
-    // para que isso ocorra na inicialização da pagina
     this.appName = 'Melhor Frete'
+    const apiUrl = 'http://localhost:3000/transport'
+
+    // Fetch nos dados da API
+    fetch (apiUrl)
+    .then (response => response.json())
+    .then(data =>{
+
+        // Retornando as cidades únicas para o objeto cidades.
+        let cidadesUnicas = {};
+        for (let i = 0; i < data.length; i++){
+          let obj = data[i];
+          cidadesUnicas[obj.city] = true;
+        }
+
+        this.cidades = Object.keys(cidadesUnicas);
+        console.log(this.cidades)
+    })
+    .catch(error =>{
+        console.error('Erro ao obter dados do frete:', error);
+      })
   },
   methods: {
     // Implemente aqui os metodos utilizados na pagina
     methodFoo() {
-      console.log(this.appName)
     },
   },
 }
